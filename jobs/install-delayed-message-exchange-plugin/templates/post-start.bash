28dc2989af4725855d4ec49f9b5ad5e9e76228c8
Eric Brusseau <eric@brusseau.org>
2019-01-25 18:29:28 -0800

    Added removal of existing plugin file before copying from release package

--- jobs/install-delayed-message-exchange-plugin/templates/post-start.bash
+++ jobs/install-delayed-message-exchange-plugin/templates/post-start.bash
@@ -6,6 +6,7 @@ PATH=$PATH:/var/vcap/packages/erlang/bin
 
 . /var/vcap/jobs/rabbitmq-server/lib/rabbitmq-config-vars.bash
 
+rm -f $RMQ_PACKAGE_PATH/plugins/rabbitmq_delayed_message_exchange*.ez
 unzip $PLUGIN_PACKAGE_PATH/rabbitmq-delayed-message-exchange-plugin-$RMQ_SERVER_VERSION.x.zip -d $RMQ_PACKAGE_PATH/plugins
 
 $RMQ_PACKAGE_PATH/bin/rabbitmq-plugins enable rabbitmq_delayed_message_exchange
